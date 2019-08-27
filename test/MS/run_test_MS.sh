8937685d0588f1e00d52c8b8ccd9a2d101378044
anhoang <anhoang@wi.mit.edu>
2019-08-27 11:12:23 -0400

    add MS test case

---
+++ test/MS/run_test_MS.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../../python_scripts/orchestrator_an.py run_fresh /lab/corradin_data/FOR_AN/OUTSIDE_VARIANT_PIPELINE/github_repos/outside-variants/test/MS/input_folder ALL_MS_impute2_chr16.gen ALL_controls_58C_NBS_WTC2_impute2_chr16.gen test_MS Anna_init_get_mtc_OR1.txt test_MS --override -g /MS_logreg
